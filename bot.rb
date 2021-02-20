fbf26e065b99ed6092b00db6514b316068cba23f
GBraga23 <gbfgbf1000@gmail.com>
2021-02-20 04:09:13 +0000

    Add files via upload

---
+++ bot.rb
@@ -0,0 +1,6 @@
+source 'https://rubygems.org'
+gem 'telegram_bot'
+require 'telegram_bot'
+token = '1694196880:AAFMZb-2Te8sUKoiwSy581X0e4s_yPM9CzM'
+bot = TelegramBot.new(token: token)
+reply.text = "OLA"
